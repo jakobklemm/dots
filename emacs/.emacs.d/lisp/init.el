b045923897650cbc4ac622120cef34cb5bbcc94f
Jakob Klemm <github@jeykey.net>
2021-12-29 15:49:48 +0100

    Restructured into all lisp.

--- emacs/.emacs.d/lisp/init.el
+++ emacs/.emacs.d/lisp/init.el
@@ -10,28 +10,28 @@
                      gcs-done)))
 
 ;; Initial settings, disable some emacs features.
-(load-file (concat user-emacs-directory "lisp/setup.el"))
+(load-file (concat user-emacs-directory "modules/setup.el"))
 
 ;; Package installers
-(load-file (concat user-emacs-directory "lisp/packages.el"))
+(load-file (concat user-emacs-directory "modules/packages.el"))
 
 ;; Basic packages, util stuff.
-(load-file (concat user-emacs-directory "lisp/core.el"))
+(load-file (concat user-emacs-directory "modules/core.el"))
 
 ;; Theme & Colors
-(load-file (concat user-emacs-directory "lisp/design.el"))
+(load-file (concat user-emacs-directory "modules/design.el"))
 
 ;; Moving between files, buffers and frames
-(load-file (concat user-emacs-directory "lisp/navigation.el"))
+(load-file (concat user-emacs-directory "modules/navigation.el"))
 
 ;; org-mode & writing
-(load-file (concat user-emacs-directory "lisp/org.el"))
+(load-file (concat user-emacs-directory "modules/org.el"))
 
 ;; Git, Programming & Modes
-(load-file (concat user-emacs-directory "lisp/programming.el"))
+(load-file (concat user-emacs-directory "modules/programming.el"))
 
 ;; Keybinds, evil stuff
-(load-file (concat user-emacs-directory "lisp/evil.el"))
+(load-file (concat user-emacs-directory "modules/evil.el"))
 
 ;; Any custom stuff
 (setq custom-file (concat user-emacs-directory "custom.el"))
