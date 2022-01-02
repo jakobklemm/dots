6fd8b6985893a1d306712f4552521e5b210b5347
Jakob Klemm <github@jeykey.net>
2022-01-02 23:59:31 +0100

    Cleared comments from init.

--- emacs/.emacs.d/lisp/init.el
+++ emacs/.emacs.d/lisp/init.el
@@ -11,28 +11,13 @@
 
 ;; Initial settings, disable some emacs features.
 (load-file (concat user-emacs-directory "modules/setup.el"))
-
-;; Package installers
 (load-file (concat user-emacs-directory "modules/packages.el"))
-
-;; Basic packages, util stuff.
 (load-file (concat user-emacs-directory "modules/core.el"))
-
-;; Theme & Colors
 (load-file (concat user-emacs-directory "modules/design.el"))
-
-;; Moving between files, buffers and frames
 (load-file (concat user-emacs-directory "modules/navigation.el"))
-
-;; org-mode & writing
 (load-file (concat user-emacs-directory "modules/org.el"))
-
-;; Git, Programming & Modes
 (load-file (concat user-emacs-directory "modules/programming.el"))
-
-;; Keybinds, evil stuff
 (load-file (concat user-emacs-directory "modules/evil.el"))
-
 ;; Any custom stuff
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
