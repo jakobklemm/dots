0177448d2d21239ac4fc0c77394a86068dadd34a
Jakob Klemm <github@jeykey.net>
2022-01-10 22:22:18 +0100

    Added custom caldav version for VTODO support.

--- emacs/.emacs.d/lisp/org-sync.el
+++ emacs/.emacs.d/lisp/org-sync.el
@@ -3,11 +3,10 @@
 (require 'auth-source)
 (setq auth-sources '("~/.authinfo.gpg" "~/.authinfo" "~/.netrc"))
 
-(use-package org-caldav
-  :config
-  (setq org-caldav-url "https://caldav.jeykey.net/dav.php/calendars/jeykey/")
-  (setq org-caldav-calendar-id "jeykey-personal")
-  (setq org-caldav-inbox "~/supervisor/inbox.org")
-  (setq org-caldav-files '("~/supervisor/active.org" "~/supervisor/events.org"))
-  (setq org-icalendar-timezone "Europe/Berlin")
-  )
+(require 'org-caldav)
+
+(setq org-caldav-url "https://caldav.jeykey.net/dav.php/calendars/jeykey/")
+(setq org-caldav-calendar-id "jeykey-personal")
+(setq org-caldav-inbox "~/supervisor/inbox.org")
+(setq org-caldav-files '("~/supervisor/active.org" "~/supervisor/events.org"))
+(setq org-icalendar-timezone "Europe/Berlin")
