54a5f491460db27508959cb695f245490773b583
Jakob Klemm <github@jeykey.net>
2022-01-23 22:21:45 +0100

    Updated org-publish settings to standalone.

--- emacs/.emacs.d/lisp/org-site.el
+++ emacs/.emacs.d/lisp/org-site.el
@@ -1,7 +1,6 @@
 ;; Personal website
 
 (require 'ox-publish)
-
 (use-package htmlize)
 
 (setq org-publish-project-alist
@@ -11,27 +10,53 @@
              :base-directory "~/documents/jeykey.net/content"
              :with-author nil
              :with-creator nil
+             :with-date t
              :with-toc nil
              :with-section-numbers nil
              :time-stamp-file nil
              :section-numbers nil
+             :auto-sitemap t
+             :sitemap-sort-files 'anti-chronologically
+             :sitemap-file-entry-format "%t - %d "
+             :sitemap-filename "sitemap.org"
+             :sitemap-title "Sitemap"
              :publishing-directory "~/documents/jeykey.net/public/"
              :publishing-function 'org-html-publish-to-html
              )
        (list "static"
-             :base-directory "~/documents/jeykey.net/content/static"
-             :base-extension "css\\|js\\|png\\|..."
+             :base-directory "~/documents/jeykey.net/content/static/"
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|asc"
              :publishing-directory "~/documents/jeykey.net/public/static/"
              :publishing-function 'org-publish-attachment
-             :recursive t)))
+             :recursive t)
+       (list "style"
+             :base-directory "~/documents/jeykey.net/style/"
+             :base-extension "css\\|js"
+             :publishing-directory "~/documents/jeykey.net/public/style/"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+       ))
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
+      org-src-fontify-natively t
+      org-src-tab-acts-natively t
       org-html-head "
-<link rel=\"stylesheet\" href=\"style.css\" />
-<h1><a href=\"/\"> Jakob Klemm</a></h1>
+<meta name=\"author\" content=\"Jakob Klemm\">
+<link rel=\"stylesheet\" href=\"style/style.css\" />
+<h1><a href=\"/\">Jakob Klemm</a></h1>
+<div class=\"hdr\">
+<a href=\"https://github.com/jakobklemm\" target=\"_blank\">Github</a>
+<a href=\"sitemap.html\">Sitemap</a>
+<a href=\"static/public.asc\">PGP Key</a>
+<a href=\"mailto:github@jeykey.net\">github@jeykey.net</a>
+</div>
 ")
 
-(org-publish-remove-all-timestamps)
-(setq org-src-fontify-natively t)
+(defun jk/publish-site ()
+  (interactive)
+  (delete-directory "~/documents/jeykey.net/public/" t)
+  (org-publish-remove-all-timestamps)
+  (org-publish-all t)
+  )
