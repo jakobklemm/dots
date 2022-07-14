f12b0b3026bd671aa5fc624e4946638cc9620016
Jakob Klemm <github@jeykey.net>
2022-07-14 19:15:01 +0200

    Added site setup.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -28,7 +28,7 @@
   "Path to the custom modules of the config."
   )
 
-(defvar jk/active-modules '(base input windows apperance navigation programming rust writing)
+(defvar jk/active-modules '(base input windows apperance navigation programming rust writing site)
   "List of all currently active modules, get loaded at startup."
   )
 
