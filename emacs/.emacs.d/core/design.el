c156ff5e0197216884f22b71d3019a635a138fda
Jakob <jakob@jeykey.net>
2023-04-26 23:58:49 +0200

    Added Beacon mode.

--- emacs/.emacs.d/core/design.el
+++ emacs/.emacs.d/core/design.el
@@ -10,4 +10,17 @@
   (doom-modeline-mode)
   )
 
+(use-package rainbow-delimiters
+  :hook
+  ((org-mode . rainbow-delimiters-mode)
+   (prog-mode . rainbow-delimiters-mode)))
+
+(use-package beacon
+  :custom
+  ((beacon-blink-when-window-scrolls nil)
+   )
+  :hook
+  ((after-init . beacon-mode))
+  )
+
 (provide 'design)
