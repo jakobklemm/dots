48b30296e1bd50227449ce0aed71b40b8d07f12f
Jakob Klemm <jakob@jeykey.net>
2024-02-16 00:07:54 +0100

    Fixed spell checking issues (probably).

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -59,25 +59,6 @@
   (vertico-posframe-mode 1)
   )
 
-;; Configure `LANG`, otherwise ispell.el cannot find a 'default
-;; dictionary' even though multiple dictionaries will be configured
-;; in next line.
-;; (setenv "LANG" "en_US.UTF-8")
-;; (setq ispell-program-name "hunspell")
-;;   ;; Configure German, Swiss German, and two variants of English.
-;; (setq ispell-dictionary "de_DE,de_CH,en_GB,en_US")
-;;   ;; ispell-set-spellchecker-params has to be called
-;;   ;; before ispell-hunspell-add-multi-dic will work
-;; ;; (ispell-set-spellchecker-params)
-;;   ;; (ispell-hunspell-add-multi-dic "de_DE,de_CH,en_GB,en_US")
-;;   ;; For saving words to the personal dictionary, don't infer it from
-;;   ;; the locale, otherwise it would save to ~/.hunspell_de_DE.
-;; (setq ispell-personal-dictionary "~/.hunspell_personal")
-;; ;; The personal dictionary file has to exist, otherwise hunspell will
-;; ;; silently not use it.
-;; (unless (file-exists-p ispell-personal-dictionary)
-;;   (write-region "" nil ispell-personal-dictionary nil 0))
-
 (setq langtool-http-server-host "172.16.110.22"
       langtool-http-server-port 8010)
 
