90a990a3c2c3a65aae43795d7400ffa919f26996
Jakob Klemm <jakob@jeykey.net>
2024-05-13 01:18:09 +0200

    Switch to lazy, nvim rewrite + chatgtp ollama working.

--- nvim/.config/nvim/lua/plugins/color.lua
+++
@@ -1,9 +0,0 @@
--- Color stuff
-
-vim.opt.background = "dark" -- set this to dark or light
-vim.cmd.colorscheme "oxocarbon"
-
-require("notify").setup({
-       background_colour = "#000000"
-		       })
-
