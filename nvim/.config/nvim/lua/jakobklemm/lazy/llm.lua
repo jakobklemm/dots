474742c8acd000c247f17b2ee3e6f12f9dfc7eda
Jakob Klemm <git@jeykey.net>
2025-01-19 13:26:26 +0100

    add: codecompanion.nvim for reasonably good LLM integration in nvim
    still not perfect, but it actually works with Ollama

--- nvim/.config/nvim/lua/jakobklemm/lazy/llm.lua
+++ nvim/.config/nvim/lua/jakobklemm/lazy/llm.lua
@@ -1,19 +1,53 @@
 return {
-    "huynle/ogpt.nvim",
-    event = "VeryLazy",
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
     opts = {
-        default_provider = "ollama",
-        providers = {
-            ollama = {
-                api_host = "http://172.16.110.32:11434",
-                api_key = "",
-                model = "llama3.2:3b",
+        adapters = {
+            ollama = function()
+                return require("codecompanion.adapters").extend("ollama", {
+                    env = {
+                        url = "http://172.16.110.32:11434",
+                    },
+                    parameters = {
+                        sync = true,
+                    },
+                    schema = {
+                        model = {
+                            -- default = "llama3.2:3b",
+                            -- default = "llama3.1:latest",
+                            default = "codellama:latest",
+                        },
+                    },
+                })
+            end,
+        },
+        strategies = {
+            chat = {
+                adapter = "ollama",
+                keymaps = {
+                    send = {
+                        modes = {
+                            n = "<C-x>",
+                            i = "<C-x>",
+                        },
+                    },
+                    close = {
+                        modes = {
+                            n = "<C-c>",
+                            i = "<C-c>",
+                        },
+                    },
+                },
+            },
+            inline = {
+                adapter = "ollama",
+            },
+            cmd = {
+                adapter = "ollama",
             },
         },
-    },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
     },
 }
